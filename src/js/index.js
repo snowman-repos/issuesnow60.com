e21f47ae1350597c5af9670ae1e8b8310abb5689
darryl-snow <dazsnow@gmail.com>
2018-06-21 00:33:17 +0800

    image rotation

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,42 @@
 /* eslint-env browser */
 
-const myInitCode = () => {
-  const images = document.querySelectorAll('.js-image');
+const numberOfImages = 26;
+let images = [];
+
+const getRandomImage = () => Math.ceil(Math.random() * numberOfImages);
+
+const swapRandomImage = () => {
+  const image = Math.ceil(Math.random() * images.length);
+  images[image].classList.add('u-fade-out');
+  images[image].style.backgroundImage = `url(/img/mum-${getRandomImage()}.jpg)`;
+  setTimeout(() => {
+    images[image].classList.remove('u-fade-out');
+  }, 1000);
+};
+
+const rotateImages = () => {
+  const interval = Math.round(Math.random() * 2500);
+  setTimeout(() => {
+    swapRandomImage();
+    rotateImages();
+  }, interval);
+};
+
+const initialize = () => {
+  images = document.querySelectorAll('.js-image');
   for (let i = 0; i < images.length; i += 1) {
-    images[i].style.backgroundImage = `url(/img/mum-${i + 1}.jpg)`;
-    images[i].style.opacity = 1;
+    images[i].style.backgroundImage = `url(/img/mum-${getRandomImage()}.jpg)`;
+    images[i].classList.remove('u-fade-out');
   }
+  rotateImages();
 };
 
 console.log(document.readyState);
 
 if (document.readyState !== 'loading') {
-  myInitCode();
+  initialize();
 } else {
   document.addEventListener('DOMContentLoaded', () => {
-    myInitCode();
+    initialize();
   });
 }
