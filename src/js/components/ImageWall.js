fece7a48c9d62c4ab8afb092ff4c4d94651f9de2
darryl-snow <dazsnow@gmail.com>
2018-07-10 22:24:16 +0800

    add old photos

--- src/js/components/ImageWall.js
+++ src/js/components/ImageWall.js
@@ -5,7 +5,7 @@ export default class ImageWall {
     this.props = {
       imageFadeClass: 'u-fade-out',
       imageTransitionTime: 1000,
-      numberOfImages: 39,
+      numberOfImages: 57,
     };
     this.state = {
       images: document.querySelectorAll('.js-image'),
